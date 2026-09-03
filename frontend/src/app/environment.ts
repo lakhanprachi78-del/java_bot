079511393777810e2ed0920c191d33ea4c8fd217
Malhar Nikam <malhar.nikam@ugrocapital.com>
2026-09-03 15:31:44 +0530

    Added frontend application

---
+++ frontend/src/app/environment.ts
@@ -0,0 +1,15 @@
+export interface Environment {
+  production: boolean;
+  sessionId: string;
+  losWsUrl: string;
+  skaleupWsUrl: string;
+  thinkingAfterMs: number;
+}
+
+export const environment: Environment = {
+  production: false,
+  sessionId: 'session-chandan',
+  losWsUrl: 'ws://127.0.0.1:5000/ws',
+  skaleupWsUrl: 'ws://127.0.0.1:8000/ws',
+  thinkingAfterMs: 4000
+};
